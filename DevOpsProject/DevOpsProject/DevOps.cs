3f7372e2671b3f8cdf23ce547d40946d51edb558
Pranshu Agarwal <pranshuagarwalpr@gmail.com>
2019-10-25 11:24:27 +0530

    commitprompt

---
+++ DevOpsProject/DevOpsProject/DevOps.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsProject
+{
+    class DevOps
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is test code for question 3");
+        }
+    }
+}
